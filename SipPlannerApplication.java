7a124d4785e082a17ce6ce29a5bc8b3ff3bbab3b
Sabari Sundaresan <sundaresansabari@gmail.com>
2025-10-07 08:39:08 +0530

    Create SipPlannerApplication.java

---
+++ SipPlannerApplication.java
@@ -0,0 +1,11 @@
+package com.sipplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipPlannerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SipPlannerApplication.class, args);
+    }
+}
